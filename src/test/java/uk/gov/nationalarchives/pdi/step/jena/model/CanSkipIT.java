4ed6871a2bf7c74b17052d644356a9ed5481a31f
Adam Retter <adam.retter@googlemail.com>
2021-02-08 16:55:09 +0100

    Rename test

--- src/test/java/uk/gov/nationalarchives/pdi/step/jena/model/CanSkipIT.java
+++ src/test/java/uk/gov/nationalarchives/pdi/step/jena/model/CanSkipIT.java
@@ -38,9 +38,11 @@ import uk.gov.nationalarchives.pdi.step.jena.serializer.JenaSerializerStepMeta;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static uk.gov.nationalarchives.pdi.step.jena.Rdf11.RDF_NAMESPACE_IRI;
 
 public class CanSkipIT {
     @SuppressWarnings("unused") // Marker field required to inject plugin
@@ -57,18 +59,22 @@ public class CanSkipIT {
     }
 
     @Test
-    public void can_create_rdf_type_statement(@TempDir final Path tempDir) throws KettleException, IOException {
-        final Path outputFilePath = tempDir.resolve("output.ttl");
+    public void skipProperties(@TempDir final Path tempDir) throws KettleException, IOException {
+        final Model expected = ModelFactory.createDefaultModel();
+        expected.add(
+                expected.createResource("http://example.com/s"),
+                expected.createProperty(RDF_NAMESPACE_IRI + "pSkipNo"),
+                expected.createLiteral("http://example.com/s"));
 
-        executeTransformation(outputFilePath);
+        final Path outputFilePath = Files.createTempFile(tempDir, "output", ".ttl");
+        executeTransformation("skipProperties.ktr", outputFilePath);
         final Model actual = loadOutputGraph(outputFilePath);
-        final Model expected = createExpectedModel();
 
         assertTrue(actual.isIsomorphicWith(expected));
     }
 
-    private static void executeTransformation(final Path outputFilePath) throws IOException, KettleException {
-        try (final InputStream ktr = EndToEndIT.class.getResourceAsStream("can_skip.ktr")) {
+    private static void executeTransformation(final String kettleTransformation, final Path outputFilePath) throws IOException, KettleException {
+        try (final InputStream ktr = EndToEndIT.class.getResourceAsStream(kettleTransformation)) {
             final Trans trans = new Trans(new TransMeta(ktr, null, false, null, null));
 
             trans.setVariable("Filename", outputFilePath.toString());
@@ -82,15 +88,5 @@ public class CanSkipIT {
         output.read(outputFilePath.toUri().toString(), "TURTLE");
 
         return output;
-    }
-
-    private static Model createExpectedModel() {
-        final Model expected = ModelFactory.createDefaultModel();
-        expected.add(
-                expected.createResource("http://example.com/s"),
-                expected.createProperty("http://www.w3.org/1999/02/22-rdf-syntax-ns#pSkipNo"),
-                expected.createLiteral("http://example.com/s"));
-
-        return expected;
     }
 }
